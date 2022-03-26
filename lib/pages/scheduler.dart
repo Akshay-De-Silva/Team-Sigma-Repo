b032c601e0004c1eafa1e6adcc99dc5ed5c03038
Dhasanthan <85722388+Dhasanthan@users.noreply.github.com>
2022-03-26 14:34:20 +0530

    adding the on/off button ,brightness level and colours for the scheduler page

--- lib/pages/scheduler.dart
+++ lib/pages/scheduler.dart
@@ -25,6 +25,10 @@ class schedulerpage extends StatefulWidget {
 class _schedulerpage extends State<schedulerpage> {
   //DateTime date = DateTime(2022, 12, 24);
   TimeOfDay time = TimeOfDay(hour: 10, minute: 30);
+  String On = "OFF";
+  bool light = false;
+  int _value = 0;
+  final dbR = FirebaseDatabase.instance.reference();
   @override
   Widget build(BuildContext context) {
     final hours = time.hour.toString().padLeft(2, '0');
@@ -100,6 +104,221 @@ class _schedulerpage extends State<schedulerpage> {
                 ],
               ),
             ),
+            FlatButton(
+              child: Image.asset(
+                "images/power2.png",
+                height: 50,
+                width: 50,
+              ), //Adding the Bulb image
+              // child:Text("LedON"),
+              //color: Colors.blue,
+              onPressed: () {
+                if (light) {
+                  //if ledstatus is true, then turn off the led
+                  //if led is on, turn off
+                  dbR.child("Light").set({"Switch": "OFF"});
+                  light = false;
+                } else {
+                  //if ledstatus is false, then turn on the led
+                  //if led is off, turn on
+                  dbR.child("Light").set({"Switch": "ON"});
+                  light = true;
+                }
+                setState(() {
+                  if (light == false) {
+                    On = "OFF";
+                    _value = 0;
+                  }
+                  if (light == true) {
+                    On = "ON";
+                  }
+                });
+              },
+            ),
+            Container(
+              child: Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                  child: light
+                      ? const Text("BULB IS ON",
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ))
+                      : const Text(
+                    "BULB IS OFF",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  )),
+            ),
+            Column(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: const <Widget>[
+                          Text(
+                            "Brightness",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ]),
+                  ),
+                ),
+                Container(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          const Icon(
+                            Icons.lightbulb_outline,
+                            color: Colors.amber,
+                            size: 30,
+                          ),
+                          Expanded(
+                            child: Slider(
+                              value: _value.toDouble(),
+                              min: 0.0,
+                              max: 100.0,
+                              divisions: 10,
+                              activeColor: Colors.black,
+                              inactiveColor: Colors.white,
+                              label: '${_value.round()}',
+                              onChanged: (double newValue) {
+                                setState(
+                                      () {
+                                    _value = newValue.round();
+                                    if (light == true) {
+                                      if (_value == 0) {
+                                        dbR.child("Light").set({"Switch": "0"});
+                                      }
+                                      if (_value == 10) {
+                                        dbR.child("Light").set({"Switch": "10"});
+                                      }
+                                      if (_value == 20) {
+                                        dbR.child("Light").set({"Switch": "20"});
+                                      }
+                                      if (_value == 30) {
+                                        dbR.child("Light").set({"Switch": "30"});
+                                      }
+                                      if (_value == 40) {
+                                        dbR.child("Light").set({"Switch": "40"});
+                                      }
+                                      if (_value == 50) {
+                                        dbR.child("Light").set({"Switch": "50"});
+                                      }
+                                      if (_value == 60) {
+                                        dbR.child("Light").set({"Switch": "60"});
+                                      }
+                                      if (_value == 70) {
+                                        dbR.child("Light").set({"Switch": "70"});
+                                      }
+                                      if (_value == 80) {
+                                        dbR.child("Light").set({"Switch": "80"});
+                                      }
+                                      if (_value == 90) {
+                                        dbR.child("Light").set({"Switch": "90"});
+                                      }
+                                      if (_value == 100) {
+                                        dbR.child("Light").set({"Switch": "100"});
+                                      }
+                                    }
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                          const Icon(
+                            Icons.lightbulb,
+                            color: Colors.amber,
+                            size: 30,
+                          ),
+                        ])),
+              ],
+            ),
+            Column(children: [
+              Container(
+                  child: Padding(
+                      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          children: const [
+                            Text(
+                              "Color",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ]))),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white54,
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                height: 30,
+                margin: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    FlatButton(
+                      child: const Text(""),
+                      color: Colors.red,
+                      height: 20,
+                      minWidth: 10,
+                      onPressed: () {
+                        if (light == true) {
+                          dbR.child("Light").set({"Switch": "red"});
+                        }
+                      },
+                    ),
+                    FlatButton(
+                      child: const Text(""),
+                      color: Colors.green,
+                      height: 20,
+                      minWidth: 10,
+                      onPressed: () {
+                        if (light == true) {
+                          dbR.child("Light").set({"Switch": "green"});
+                        }
+                      },
+                    ),
+                    FlatButton(
+                      child: const Text(""),
+                      color: Colors.blue,
+                      height: 20,
+                      minWidth: 10,
+                      onPressed: () {
+                        if (light == true) {
+                          dbR.child("Light").set({"Switch": "blue"});
+                        }
+                      },
+                    ),
+                    FlatButton(
+                      child: const Text(""),
+                      color: Colors.yellow,
+                      height: 20,
+                      minWidth: 10,
+                      onPressed: () {
+                        if (light == true) {
+                          dbR.child("Light").set({"Switch": "yellow"});
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ]),
           ],
         ),
       ),
